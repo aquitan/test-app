2e71f4f823de4cf9b68db9c190fa0f571e6affbd
aquitan <aquitan@mail.ru>
2025-03-07 23:19:34 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+import { Square } from './components';
+
+function App() {
+
+  return <div className={styles.app}>
+		<Square />
+	</div>;
+}
+
+export default App
---
+++ src/components/square/Square.tsx
@@ -0,0 +1,10 @@
+import styles from './Square.module.css';
+
+const Square = () => {
+	return (
+		<div className={styles.square}>
+		</div>
+	)
+}
+
+export {Square};
